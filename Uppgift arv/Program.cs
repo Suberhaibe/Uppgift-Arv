bd871f53b1c3e4e8a7720f9892027036fbea2cc6
Adam <adfr06001@utb.vaxjo.se>
2023-09-14 10:20:41 +0200

    Klar med uppgift arv

--- Uppgift arv/Program.cs
+++ Uppgift arv/Program.cs
@@ -4,25 +4,22 @@ using Uppgift_arv;
 
 Console.WriteLine("Skriv 1 för rätvinklig triangel och 2 för rektangel");
 int val = int.Parse(Console.ReadLine());
-
+Shape shape = null;
 
 if (val == 2)
 {
     Console.WriteLine("Skriv höjd och bredd på var enda linje");
-    Shape p1 = new Rectangle(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())); //
-
-    Console.WriteLine("Area är: " +p1.Area()+ ".");
-    Console.WriteLine("Omkrets är: " +p1.Circumferance()+ ".");
+    shape = new Rectangle(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())); //
 }
 else if(val== 1)
 {
     Console.WriteLine("Skriv höjd och bredd på var enda linje");
-    Triangle p2 = new Triangle(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
-
-    Console.WriteLine("Area är: " +p2.Area()+ ".");
-    Console.WriteLine("Omkrets är: " +p2.Circumferance()+ ".");
+    shape = new Triangle(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
 }
 else
 {
     Console.WriteLine("Välj mellan 1 eller 2");
 }
+
+    Console.WriteLine("Area är: " +shape.Area()+ ".");
+    Console.WriteLine("Omkrets är: " +shape.Circumferance()+ ".");
